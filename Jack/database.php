753cc5034b9a96e775def9889602033e274cbad1
DRJ31 <dengrenjie31@gmail.com>
2017-03-22 23:13:34 +0800

    get ua

--- Jack/database.php
+++ Jack/database.php
@@ -1,4 +1,18 @@
 <?php
+function getClientIP()
+{
+    global $ip;
+    if (getenv("HTTP_CLIENT_IP"))
+        $ip = getenv("HTTP_CLIENT_IP");
+    else if(getenv("HTTP_X_FORWARDED_FOR"))
+        $ip = getenv("HTTP_X_FORWARDED_FOR");
+    else if(getenv("REMOTE_ADDR"))
+        $ip = getenv("REMOTE_ADDR");
+    else $ip = "Unknow";
+    return $ip;
+}
+$ipadd=getClientIP();
+$ua=$_SERVER['HTTP_USER_AGENT'];
 $arr=array();
 $dbhost='';//host
 $dbuser='';//username
@@ -26,4 +40,14 @@ for ($i=0;$i<count($arr);$i++){
         echo "[".$arr[$i][0].",".$arr[$i][1]."],";
     }
 }
+$insertdata="INSERT INTO userinfo".
+    "(useragent, ipaddress, date, time)".
+    "VALUES".
+    "('$ua','$ipadd',NOW(),NOW())";
+mysqli_select_db($conn,'demonist');
+$retval=mysqli_query($conn,$insertdata);
+if(!$retval){
+    die('Could not insert data'.mysqli_error($conn));
+}
+echo "Success!";
 mysqli_close($conn);
